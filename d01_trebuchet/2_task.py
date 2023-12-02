755aff19026f1e2348b0c30ae8daf4f071299d1c
QuiteClose <dean@quiteclose.net>
2023-12-02 02:05:49 +0000

    No need for tuple logic.

--- d01_trebuchet/2_task.py
+++ d01_trebuchet/2_task.py
@@ -6,27 +6,26 @@ NUMBERS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine
 
 def scan(given):
     '''If the given string STARTS WITH any term in DECIMALS or
-    NUMBERS, return the term and its integer value as a tuple.'''
+    NUMBERS, return a single-character string of that term.'''
     for source in [DECIMALS, NUMBERS]:
         for term in source:
             if given.startswith(term):
-                return term, source.index(term)+1
-    return '', None
+                return str(source.index(term)+1)
 
 
 def to_digits(given):
     digits = []
     while given:
-        match, value = scan(given)
+        match = scan(given)
         if match:
-            digits.append(value)
+            digits.append(match)
         given = given[1:]
     return digits
 
 
 def decode(line):
     digits = to_digits(line)
-    return int(str(digits[0])+str(digits[-1]))
+    return int(digits[0]+digits[-1])
 
 
 print(sum(map(decode, sys.stdin)))
