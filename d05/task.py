5b25febf6edc258c8031a1e06d7ada739e0a6734
QuiteClose <dean@quiteclose.net>
2023-12-05 08:50:21 +0000

    Day 5 task 2

--- d05/task.py
+++ d05/task.py
@@ -1,10 +1,10 @@
 
 from collections import namedtuple
-from functools import partial
 import sys
 
 
 Mapping = namedtuple('Mapping', 'destination source magnitude')
+ValueRange = namedtuple('ValueRange', 'start end magnitude')
 
 
 def read_until_empty_line(stream):
@@ -24,6 +24,18 @@ def read_seeds_task_one(stream):
     return [int(n) for n in numbers.strip().split()]
 
 
+def chunks(iterable, size):
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i+size]
+
+
+def read_seeds_task_two(stream):
+    return [
+        ValueRange(start, start+magnitude, magnitude)
+        for start, magnitude in chunks(read_seeds_task_one(stream), 2)
+    ]
+
+
 def parse_line(line):
     destination, source, magnitude = line.strip().split()
     return Mapping(int(destination), int(source), int(magnitude))
@@ -34,7 +46,7 @@ def read_sections(stream):
         blob = read_until_empty_line(stream)
         if not blob:
             return
-        blob.pop(0)
+        print(blob.pop(0))
         yield list(map(parse_line, blob))
 
 
@@ -45,17 +57,69 @@ def transform_one(mapping, n):
     return n
 
 
-def solution(stream, *data):
-    for mapping in read_sections(stream):
-        transform = partial(transform_one, mapping)
-        data = map(transform, data)
-    return sorted(data)[0]
+def transform_range(mapping, value_range):
+    index = value_range.start
+    for destination, source, magnitude in sorted(mapping, key=lambda x: x.source):
+        offset = destination-source
+        if index < source and value_range.end < source:
+            yield ValueRange(index, value_range.end, value_range.end-index)
+            return
+        elif index < source and source < value_range.end <= source+magnitude:
+            yield ValueRange(index, source, source-index)
+            yield ValueRange(
+                source+offset,
+                value_range.end+offset,
+                value_range.end-source,
+            )
+            return
+        elif index < source and value_range.end >= source+magnitude:
+            yield ValueRange(index, source, source-index)
+            yield ValueRange(
+                source+offset,
+                source+magnitude+offset,
+                magnitude,
+            )
+            index = source+magnitude
+        elif source <= index < source+magnitude and value_range.end <= source+magnitude:
+            yield ValueRange(
+                index+offset,
+                value_range.end+offset,
+                value_range.end-index,
+            )
+            return
+        elif source <= index < source+magnitude and value_range.end > source+magnitude:
+            yield ValueRange(
+                index+offset,
+                source+magnitude+offset,
+                source+magnitude-index,
+            )
+            index = source+magnitude
+    else:
+        yield ValueRange(index, value_range.end, value_range.end-index)
 
 
 def task_one(stream):
-    seeds = read_seeds_task_one(stream)
-    return solution(stream, *seeds)
+    data = read_seeds_task_one(stream)
+    for mapping in read_sections(stream):
+        data = transform_one(mapping, data)
+    return sorted(data)[0]
 
 
-print(task_one(sys.stdin))
+def task_two_solution(mapping, data):
+    result = []
+    for value_range in data:
+        for output in transform_range(mapping, value_range):
+            result.append(output)
+    return result
+
+
+def task_two(stream):
+    data = read_seeds_task_two(stream)
+    for mapping in read_sections(stream):
+        data = task_two_solution(mapping, data)
+    return sorted(data, key=lambda x: x.start)[0].start
+
+
+# print(task_one(sys.stdin))
+print(task_two(sys.stdin))
 
